8d9b62ceb9bf91ad3309511b6e07578ec5f0b4f5
nilsmango <simon@nilsmango.ch>
2022-12-28 14:38:49 +0100

    Initial Commit

---
+++ Nightlight/NightlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightlightApp.swift
+//  Nightlight
+//
+//  Created by Simon Lang on 28.12.22.
+//
+
+import SwiftUI
+
+@main
+struct NightlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
